5dbef9355082a91dc2d10e6b5adcba54059df337
matheusfgomes <matheusfgomes935@gmail.com>
2019-12-03 19:06:30 -0300

    2 files modified, 2 added

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   procution: false,
   firebase: {
-    apiKey: "AIzaSyA0QRbl7XoR8Fs6SAfucrBr9g2lXnvVOD4",
+   apiKey: "AIzaSyA0QRbl7XoR8Fs6SAfucrBr9g2lXnvVOD4",
     authDomain: "bd-cadastroaluno.firebaseapp.com",
     databaseURL: "https://bd-cadastroaluno.firebaseio.com",
     projectId: "bd-cadastroaluno",
     storageBucket: "bd-cadastroaluno.appspot.com",
     messagingSenderId: "479805357421",
     appId: "1:479805357421:web:03b03d0b0930d299d56e64"
-  
   }
 };
